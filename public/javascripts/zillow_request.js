e2fe65843f5603cd611d4f813f4b9151e1ea3ea7
DavidBonaroti <dbonaroti@gmail.com>
2017-02-10 02:33:53 -0500

    2nd Commit

--- public/javascripts/zillow_request.js
+++ public/javascripts/zillow_request.js
@@ -1,19 +1,19 @@
-console.log('listings', listings);
-const API_KEY = "X1-ZWz19eqbfoa0i3_2jayc";
-const ROOT_URL = "https://crossorigin.me/http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=" + API_KEY;
-var url = ROOT_URL + "&address=" + zillowAddress + "&citystatezip=" + city + "%2C+PA"; // change template strings (es6)
-
-
-$(function () {
-  console.log($('zillow-btn'))
-  $('button.zillow-btn').on('click', function(){
-    $('.btn btn-success zillow-btn').attr('data-zillow');
+$(document).ready(function () {
+  $('#button').on('click', function(){
+    var address = $(this).attr('data-zillow');
+    var city = $(this).attr('data-city');
+    console.log(address);
+    console.log(city);
+    const API_KEY = "X1-ZWz19eqbfoa0i3_2jayc";
+    const ROOT_URL = "https://www.zillow.com/webservice/GetSearchResults.htm?zws-id=" + API_KEY;
+    var url = ROOT_URL + "&address=" + address + "&citystatezip=" + city + "%2C+PA";
+    console.log(url);
     $.ajax({
       type: 'GET',
-      url: $(this).getDataAttribute('listing-url'),
-      success: function(listingData) {
-        console.log('listingData', data);
-        $('.listing-information').append('<div class="zillowInfo"></div>');
+      url: url,
+      success: function(zillowInfo) {
+        console.log('zillowInfo', zillowInfo);
+        //$('.listing-information').append('<div class="zillowInfo"></div>');
       }
     });
   });
